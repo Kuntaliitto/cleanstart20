23f3204aa44793c6149f4e893c73bc4af4a4e449
Antti Tuppurainen <antti.tuppurainen@industry62.com>
2018-10-25 14:52:07 +0300

    Missing vendor

---
+++ vendor/teamdeeson/wardenapi/README.md
@@ -0,0 +1,3 @@
+# Warden API
+
+An API for communicating with the [Warden Server](https://github.com/teamdeeson/warden) Application
---
+++ vendor/teamdeeson/wardenapi/tests/ApiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WardenApi;
+
+/**
+ * Class to test the Warden API functionality.
+ *
+ * @package WardenApi
+ * @author John Ennew <johne@deeson.co.uk>
+ */
+class ApiTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * @Test
+   * Test the basic operation to create a Warden API connection.
+   */
+  public function testCreate() {
+    $api = new Api('http://www.example.com', 'user', 'pass', '/dev/null');
+
+    $this->assertEquals('http://www.example.com', $api->getWardenUrl());
+    $this->assertEquals('user', $api->getUsername());
+    $this->assertEquals('pass', $api->getPassword());
+    $this->assertEquals('/dev/null', $api->getCertificatePath());
+    $this->assertEquals(TRUE, $api->hasBasicAuthentication());
+    $this->assertEquals(TRUE, $api->hasCertificatePath());
+  }
+
+}
